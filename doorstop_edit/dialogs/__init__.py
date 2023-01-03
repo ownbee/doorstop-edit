76f72207d697ed26bfdbed242d59f9dcf8bb94cb
Alexander Ernfridsson <ernfridsson@gmail.com>
2023-01-03 21:37:36 +0100

    Fix code style

--- doorstop_edit/dialogs/__init__.py
+++ doorstop_edit/dialogs/__init__.py
@@ -1,5 +1,5 @@
 """Common dialogs"""
 
-from doorstop_edit.dialogs.confirm_dialog import ConfirmDialog
-from doorstop_edit.dialogs.diff_dialog import DiffDialog
-from doorstop_edit.dialogs.info_dialog import InfoDialog
+from doorstop_edit.dialogs.confirm_dialog import ConfirmDialog  # noqa
+from doorstop_edit.dialogs.diff_dialog import DiffDialog  # noqa
+from doorstop_edit.dialogs.info_dialog import InfoDialog  # noqa
